b2e184de1b1b5125b252bec85004c87365ea2611
Poonam Chand Sahu <poonamchandsahu.ssgi@gmail.com>
2021-04-17 14:03:31 +0530

    Add files via upload

---
+++ setup-env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+#sudo apt-get upgrade -y
+touch ~/.cloudshell/no-apt-get-warning
